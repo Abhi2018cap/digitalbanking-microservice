dd2cdf5748519510d2b2057229d312ca2eaaa164
Neeraj Bhatnagar <neeraj.bhatnagar2@gmail.com>
2017-01-17 12:13:44 +0530

    Updated loan service code.

--- digitalbanking-loanservice/scripts/03_insert_data.sql
+++ digitalbanking-loanservice/scripts/03_insert_data.sql
@@ -15,14 +15,18 @@ insert into customer ("customer_id", "customer_name", "address", "city", "mobile
  VALUES (2, 'Raj', 'Bandra', 'Mumbai', 86336, 'b@b.com');
 	 
 	 
-insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest")
- VALUES (1, 1, 'HL-123', '30000','HL', 1, 1,240,9.0);
+insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest",
+"loan_status","loan_commencement_date","loan_creation_date","loan_emi_date")
+ VALUES (1, 1, 'HL-123', '30000','HL', 1, 1,240,9.0,'Open','2016-03-21 09:10:59.897666','2016-04-21 09:10:59.897666','2016-05-21 09:10:59.897666');
  
- insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest")
- VALUES (2, 1, 'AL-153', '20000','AL', 2, 1,84,9.75);
+ insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest","loan_status",
+ "loan_commencement_date","loan_creation_date","loan_emi_date")
+ VALUES (2, 1, 'AL-153', '20000','AL', 2, 1,84,9.75,'Open','2016-03-21 09:10:59.897666','2016-04-21 09:10:59.897666','2016-05-21 09:10:59.897666');
  
- insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest")
- VALUES (3, 1, 'PL-154', '50000','PL', 1, 1,60,11.49);
+ insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest",
+ "loan_status","loan_commencement_date","loan_creation_date","loan_emi_date")
+ VALUES (3, 1, 'PL-154', '50000','PL', 1, 1,60,11.49,'Open','2016-03-21 09:10:59.897666','2016-04-21 09:10:59.897666','2016-05-21 09:10:59.897666');
  
- insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest")
- VALUES (4, 1, 'AL-153', '20000','AL', 2, 1,84,9.75);
+ insert into loan ("loan_account_id", "branch_id", "loan_account_number", "amount", "loan_type", "customer_id", "loan_id","duration","rate_of_interest",
+ "loan_status","loan_commencement_date","loan_creation_date","loan_emi_date")
+ VALUES (4, 1, 'AL-153', '20000','AL', 2, 1,84,9.75,'Open','2016-03-21 09:10:59.897666','2016-04-21 09:10:59.897666','2016-05-21 09:10:59.897666');
