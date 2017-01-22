597e65db8e1ce415fb077c49f9379235537fc30e
Mahesh Natarajan <umamags@gmail.com>
2017-01-22 17:07:35 +0530

    added cors for cards service

--- digitalbanking-cardsservice/src/main/java/com/capg/cardservices/App.java
+++ digitalbanking-cardsservice/src/main/java/com/capg/cardservices/App.java
@@ -6,6 +6,12 @@ import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +42,20 @@ public class App extends Application<CardConfiguration> {
 	@Override
 	public void run(CardConfiguration c, Environment e) throws Exception {
 		LOGGER.info("Registering REST resources");
-		final CardDAO dao = new CardDAO(hibernate.getSessionFactory());
+		
+	    //Enable CORS headers
+		final FilterRegistration.Dynamic cors =
+	            e.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+	    // Configure CORS parameters
+	    cors.setInitParameter("allowedOrigins", "*");
+	    cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+	    cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+	    // Add URL mapping
+	    cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");	    
+		
+	    final CardDAO dao = new CardDAO(hibernate.getSessionFactory());
 		e.jersey().register(new CardServicesController(dao));
 	}
 
