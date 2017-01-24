d2a2b983250fb8d81ef031f4b2d1d5373ae142a4
Mahesh Natarajan <umamags@gmail.com>
2017-01-24 09:28:57 +0530

    removed ui

--- digitalbanking-ui/digitalbanking-web/src/main/webapp/js/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var digitalbankingDirectives = angular.module('digitalbankingDirectives', []).config( [
-'$compileProvider',
-function( $compileProvider )
-{ 
-$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile):|data:image\//);
-}
-]);
-
